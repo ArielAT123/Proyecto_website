b41190f2611e54a3774b9f0285100c69dc6e14f3
vendieta <vendieta5@gmail.com>
2025-02-13 23:54:54 -0500

    creacion de la componente card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardProductComponent } from './card-product/card-product.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CardProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'website';
 }
