f9bb98e7d0dbb8a8865b0eb2d4547143e0fbb781
Alejandro <108953308+vendieta@users.noreply.github.com>
2025-02-19 23:00:44 +0000

    creacion las cards dinamicas de la pagina principal

--- src/app/card-product/card-product.component.ts
+++ src/app/card-product/card-product.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-product',
-  imports: [],
+  // imports: [], // Puedes eliminar esto si no usas imports
   templateUrl: './card-product.component.html',
-  styleUrl: './card-product.component.css',
+  styleUrls: ['./card-product.component.css'],
+  standalone: true,
 })
 export class CardProductComponent {
+  @Input() price : number = 0; 
+  @Input() descripcionMin : string = 'Cargando...';
+  @Input() name : string = 'Cargando...';
+  @Input() imageUrl : string = '';
 
 }
